ae35ab16f89148af94c7c1f24b955681d9777dc3
Sam Davies <sam.jc.davies@gmail.com>
2013-05-06 20:44:25 -0500

    Initial Commit

---
+++ TeamListTests/TeamListTests.h
@@ -0,0 +1,13 @@
+//
+//  TeamListTests.h
+//  TeamListTests
+//
+//  Created by Sam Davies on 06/05/2013.
+//  Copyright (c) 2013 Shinobi Controls. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TeamListTests : SenTestCase
+
+@end
---
+++ TeamListTests/TeamListTests.m
@@ -0,0 +1,32 @@
+//
+//  TeamListTests.m
+//  TeamListTests
+//
+//  Created by Sam Davies on 06/05/2013.
+//  Copyright (c) 2013 Shinobi Controls. All rights reserved.
+//
+
+#import "TeamListTests.h"
+
+@implementation TeamListTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TeamListTests");
+}
+
+@end
