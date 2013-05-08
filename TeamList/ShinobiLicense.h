89cf086c01fc85c82d52d49ae0323d4adbf5d5a2
Sam Davies <sam.jc.davies@gmail.com>
2013-05-08 22:39:18 +0100

    Adds ShinobiGrids and the licensing code

---
+++ TeamList/ShinobiLicense.h
@@ -0,0 +1,15 @@
+//
+//  ShinobiLicense.h
+//  TeamList
+//
+//  Created by Sam Davies on 06/05/2013.
+//  Copyright (c) 2013 Shinobi Controls. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ShinobiLicense : NSObject
+
++ (NSString *)getShinobiLicenseKey;
+
+@end
---
+++ TeamList/ShinobiLicense.m
@@ -0,0 +1,33 @@
+//
+//  ShinobiLicense.m
+//  TeamList
+//
+//  Created by Sam Davies on 06/05/2013.
+//  Copyright (c) 2013 Shinobi Controls. All rights reserved.
+//
+
+#import "ShinobiLicense.h"
+
+@implementation ShinobiLicense
+
++ (NSString *)getShinobiLicenseKey
+{
+    /* We've used a plist file to keep a hold of the Shinobi License key,
+     which is required when you run a demo. You can either create yourself
+     your own plist file and put the key you have been provided in there,
+     at the root level, with the key "ShinobiChartsLicenseKey", or alternatively
+     you can just make this method return it.
+     */
+    
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"ShinobiLicenseKey" ofType:@"plist"];
+    NSDictionary *settings = [[NSDictionary alloc] initWithContentsOfFile:path];
+    NSString *key = settings[@"ShinobiChartsLicenseKey"];
+    if(key && ![key isEqualToString:@""]) {
+        return key;
+    }
+    
+    // If you don't want to use the sample plist provided, pop your code below
+    return @"YOUR CODE HERE";
+}
+
+@end
